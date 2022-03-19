e72c299c0413eda26a80e3b471297d18fda78afe
cryshado <mirmiclub@gmail.com>
2022-03-19 23:01:48 +0300

    format code

--- proxy/handler.go
+++ proxy/handler.go
@@ -11,51 +11,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func readTcpBytes(conn net.Conn) ([]byte, error) {
-	buf := make([]byte, 1024)
-
-	size, err := conn.Read(buf)
-	if err != nil {
-		return nil, err
-	}
-
-	return buf[:size], nil
-}
-
-func chanFromWSConn(wsconn *websocket.Conn) chan []byte {
-	wsc := make(chan []byte)
-
-	go func() {
-		for {
-			_, wsmsg, err := wsconn.ReadMessage()
-			if err != nil {
-				wsc <- nil
-				break
-			}
-			wsc <- wsmsg
-		}
-	}()
-
-	return wsc
-}
-
-func chanFromTCPConn(tcpconn net.Conn) chan []byte {
-	tcpc := make(chan []byte)
-
-	go func() {
-		for {
-			tcpmsg, err := readTcpBytes(tcpconn)
-			if err != nil {
-				tcpc <- nil
-				break
-			}
-			tcpc <- tcpmsg
-		}
-	}()
-
-	return tcpc
-}
-
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 
 	wsconn, err := upgrader.Upgrade(w, r, nil)
---
+++ proxy/util.go
@@ -0,0 +1,52 @@
+package proxy
+
+import (
+	"net"
+
+	"github.com/gorilla/websocket"
+)
+
+func readTcpBytes(conn net.Conn) ([]byte, error) {
+	buf := make([]byte, 1024)
+
+	size, err := conn.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf[:size], nil
+}
+
+func chanFromWSConn(wsconn *websocket.Conn) chan []byte {
+	wsc := make(chan []byte)
+
+	go func() {
+		for {
+			_, wsmsg, err := wsconn.ReadMessage()
+			if err != nil {
+				wsc <- nil
+				break
+			}
+			wsc <- wsmsg
+		}
+	}()
+
+	return wsc
+}
+
+func chanFromTCPConn(tcpconn net.Conn) chan []byte {
+	tcpc := make(chan []byte)
+
+	go func() {
+		for {
+			tcpmsg, err := readTcpBytes(tcpconn)
+			if err != nil {
+				tcpc <- nil
+				break
+			}
+			tcpc <- tcpmsg
+		}
+	}()
+
+	return tcpc
+}
